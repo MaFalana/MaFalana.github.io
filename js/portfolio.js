7e99d686e0ef5636168b7518abfbc264d0c90744
Malik Falana <104946127+MaFalana@users.noreply.github.com>
2022-09-21 19:00:54 -0400

    worked on contact form

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,1 +1,10 @@
-
+/**************************************
+ TITLE: portfolio.js							
+ AUTHOR: Malik Falana (MF)			
+ CREATE DATE: 09/21/2022	
+ PURPOSE: To use jquery and functions for portfolio page
+ LAST MODIFIED ON: 09/21/2022	
+ LAST MODIFIED BY: Malik Falana (MF)
+ MODIFICATION HISTORY:
+09/21/2022: Original Build
+***************************************/
